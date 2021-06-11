ce7ef4c00e313099409c2c699d2f775585a4971c
Vortex <70918063+Vortttex@users.noreply.github.com>
2021-06-11 13:53:45 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,8 +4,6 @@ const client = global.client = _client
 const config = require("./config.json");
 const db = require('quick.db');
 const fs = require("fs");
-const monent = reqiure('monent');
-require('monent-duration-format');
 const prefix = config.Prefix;
 
 client.commands = new Discord.Collection();
